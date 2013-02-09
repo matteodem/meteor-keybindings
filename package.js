fb365e758ced410e3e6264bd129a035895c58872
Matteo De Micheli <matteo.de.micheli@liip.ch>
2013-02-09 23:28:51 +0100

    edited package.js (a comma too much)

--- package.js
+++ package.js
@@ -2,12 +2,13 @@ Package.describe({
     summary : "Keybinding package for meteor"
 });
 
-Package.on_use(function(api) {
+Package.on_use(function (api) {
     api.use('jquery', 'client');
     api.use('underscore', 'client');
 
     api.add_files([
         'lib/jwerty.min.js',
-        'lib/keybindings.min.js',]
-        , 'client');
+        'lib/keybindings.min.js'
+    ], 'client'
+    );
 });
