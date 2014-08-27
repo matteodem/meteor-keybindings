d9cb967caad257aee505677b0c565738d1d6b222
Matteo De Micheli <mat.demicheli@gmail.com>
2014-08-27 22:25:49 +0200

    Make it 0.9.0 compliant

--- package.js
+++ package.js
@@ -1,8 +1,12 @@
 Package.describe({
+    name : "matteodem:keybindings",
+    version : "0.4.1",
     summary : "Keybinding package for meteor"
 });
 
 Package.on_use(function (api) {
+    api.versionsFrom('METEOR@0.9.0');
+
     api.use('jquery', 'client');
     api.use('underscore', 'client');
 
